62b560caf53fa3ed48133a068bb51ea558fd791f
Rashed967 <rashedhasan967@gmail.com>
2023-10-26 03:53:28 +0600

    added some code

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -5,11 +5,22 @@ const About = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row">
-                <img src={person} className="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                <div className='lg:w-1/2 relative'>
+
+                <img src={person} className="w-3/4 rounded-lg shadow-2xl" />
+                <img  src={parts} className="absolute right-0 -bottom-16 border-4 border-white  w-1/2 rounded-md shadow-2xl" />
+
+                </div>
+                <div className='lg:w-1/2 text-left'>
+                    <h4 className="text-base text-red-700 font-weight font-bold">About Us</h4>
+                    <h1 className="mt-4  text-3xl font-bold w-2/3">We are qualified & of experience in this field</h1>
+                    <div className='mt-5 space-y-6 mb-8'>
+                    <p className='leading-7'>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <p>
+                    the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
+                    </p>
+                    </div>
+                    <button className="btn btn-error">Get Started</button>
                 </div>
             </div>
         </div>
